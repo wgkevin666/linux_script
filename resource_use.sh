874b1fb9f513f143c1572adf4a98bbab2ebfd7b2
wgkevin666 <38356512+wgkevin666@users.noreply.github.com>
2019-10-31 10:44:16 +0800

    Update resource_use.sh

--- resource_use.sh
+++ resource_use.sh
@@ -4,4 +4,6 @@ for ip in `cat list`
 do
 	CPU= ssh root@$ip top -bn1 | grep load | awk '{printf "CPU load: %.2f", $(NF-2)}'
 	MEM= ssh -o ConnectTimeout=3 root@$ip free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%s.2f%%)", $3,$2,$3*100/2}'
-	DISK= ssh -o ConnectTimeout=3 root@$ip df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)", $3,$2,$5}'
+	SSDDISK= ssh -o ConnectTimeout=3 root@$ip df -h | awk '$NF=="/"{printf "Disk Usage: %.0fGB/%dGB (%s)", $3,$2,$5}'
+        printf %-20s %-20s %-20s %-20s %-20s\n" "$ip",   "$CPU",   "$MEM",  "$SSDDISK"
+done
